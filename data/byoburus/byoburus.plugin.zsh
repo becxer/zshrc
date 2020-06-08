b5e4bb5f22f85fd5d60a1b57c00a6c27d9630e9e
Seohyun Back <becxer87@gmail.com>
2020-06-08 14:56:54 +0900

    Update byoburus.plugin.zsh

--- data/byoburus/byoburus.plugin.zsh
+++ data/byoburus/byoburus.plugin.zsh
@@ -61,7 +61,3 @@ toilet -f mono9 -F gay $(whoami)_
 echo -e '\033[1;36m'Concurrent users are............................'\033[0m'
 w
 echo -e '\033[1;36m'................................................'\033[0m'
-
-# Using CUDA
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
-export CUDA_HOME=/usr/local/cuda
